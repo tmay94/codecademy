8b18dcc4bb7eac0462d2179e04363313dc5e80d8
tmay94 <72574380+tmay94@users.noreply.github.com>
2021-05-07 16:25:51 -0400

    Create Errors-In-Python.py

---
+++ Python3/Projects/Errors-In-Python.py
@@ -0,0 +1,19 @@
+# Error Code
+"""
+x == 'y"
+y = +>+ .potato
+z = (print(call))
+"lala"
+"""
+#
+# "Fixed" Code
+x = 1
+y = 2
+potato = 'CAKE IS A LIE'
+call = potato
+x == 'y'
+y = potato
+z = (print(call))
+"lala"
+
+safe moon
