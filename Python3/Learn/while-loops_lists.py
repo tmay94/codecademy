b66b3d92054b192d8c277d9a2c4737f5fd8c64f1
tmay94 <72574380+tmay94@users.noreply.github.com>
2021-05-13 16:19:59 -0400

    Create while-loops_lists.py

---
+++ Python3/Learn/while-loops_lists.py
@@ -0,0 +1,8 @@
+python_topics = ["variables", "control flow", "loops", "modules", "classes"]
+
+length = len(python_topics)
+index = 0
+
+while index < length:
+  print("I am learning about " + python_topics[index])
+  index += 1
